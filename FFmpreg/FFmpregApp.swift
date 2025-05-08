274c4aa80782c422f2a6cc214140486a76bcac92
BomberFish <bomberfish@bomberfish.ca>
2025-05-08 08:31:17 -0400

    Initial Commit

---
+++ FFmpreg/FFmpregApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// FFmpregApp.swift – FFmpreg
+// created on 2025-05-08
+
+import SwiftUI
+
+@main
+struct FFmpregApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
